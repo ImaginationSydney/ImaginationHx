c32ee66251c9462b8133426aa8cdf44b0c57a2c9
Thomas Byrne <tom.byrne@imagination.com>
2016-09-18 15:08:38 +1000

    Added EXIT_FRAME signal

--- src/com/imagination/util/time/EnterFrame.hx
+++ src/com/imagination/util/time/EnterFrame.hx
@@ -68,6 +68,19 @@ class EnterFrame
 	private static var _delayPool:Array<DelayTracker>;
 	
 	#if (flash || openfl)
+	static var _exit:Signal0;
+	static public var exit(get, null):Signal0;
+	static function get_exit():Signal0 {
+		if (_exit == null){
+			setupListener();
+			shape.addEventListener(flash.events.Event.EXIT_FRAME, function(e:flash.events.Event) { _exit.dispatch(); } );
+			_exit = new Signal0();
+		}
+		return _exit;
+	}
+	#end
+	
+	#if (flash || openfl)
 	private static var shape:Shape;
 	#end
 	

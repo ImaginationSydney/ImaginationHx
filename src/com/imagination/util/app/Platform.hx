f8364cd7ab11611fa9d7107f02189e3f067ea41b
Tom Byrne <tom.byrne@imagination.com>
2016-12-05 10:20:56 +1100

    -

--- src/com/imagination/util/app/Platform.hx
+++ src/com/imagination/util/app/Platform.hx
@@ -1,63 +1,24 @@
 package com.imagination.util.app;
 
-#if openfl
-import openfl.system.Capabilities;
-#elseif flash
-import flash.system.Capabilities;
-#end
-
-
 /**
  * ...
  * @author Thomas Byrne
  */
+#if html5
+typedef RealPlatform = BrowserPlatform;
+#else
+typedef RealPlatform = DesktopPlatform;
+#end
+
 class Platform
 {
-	static var _inited:Bool;
-	static var _isWindows:Bool;
-	static var _isMac:Bool;
-	static var _is64Bit:Bool;
-
-
-	static public function init() 
-	{
-		if (_inited) return;
-		
-		_inited = true;
-		
-		#if sys
-		var os = Sys.systemName();
-		_isWindows = (os.indexOf("Win") != -1);
-		_isMac = (os.indexOf("Mac") != -1);
-		
-		_is64Bit = _isMac;
-		
-		#else
-		
-		_is64Bit = Capabilities.supports64BitProcesses;
-		
-		var os = Capabilities.os;
-		_isWindows = (os.indexOf("Win")!=-1);
-		_isMac = (os.indexOf("Mac") != -1);
-		#end
-	}
-	
-	
 	public static function isWindows():Bool 
 	{
-		init();
-		return _isWindows;
+		return RealPlatform.isWindows();
 	}
 	
 	public static function isMac():Bool 
 	{
-		init();
-		return _isMac;
-	}
-	
-	public static function is64Bit():Bool 
-	{
-		init();
-		return _is64Bit;
+		return RealPlatform.isMac();
 	}
 }
